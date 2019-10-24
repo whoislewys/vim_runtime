a4d0e926ea06b2d16f1ba1f77bf9af08d3a7ee8b
whoislewys <whoislewys@gmail.com>
2019-10-24 11:34:02 -0700

    update install script to go to ~/.config/nvim/

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 set -e
 
-cd ~/.vim_runtime
 
-echo 'set runtimepath+=~/.vim_runtime
+cd ~/.config
 
-source ~/.vim_runtime/vimrcs/basic.vim
-source ~/.vim_runtime/vimrcs/filetypes.vim
-source ~/.vim_runtime/vimrcs/plugins_config.vim
-source ~/.vim_runtime/vimrcs/extended.vim
-source ~/.vim_runtime/vimrcs/vimplugrc.vim
+echo 'set runtimepath+=~/.config/nvim/.vim_runtime
+let &packpath=&runtimepath
 
-try
-source ~/.vim_runtime/my_configs.vim
-catch
-endtry' > ~/.vimrc
+source ~/.config/nvim/.vim_runtime/vimrcs/basic.vim
+source ~/.config/nvim/.vim_runtime/vimrcs/filetypes.vim
+source ~/.config/nvim/.vim_runtime/vimrcs/plugins_config.vim
+source ~/.config/nvim/.vim_runtime/vimrcs/extended.vim
+source ~/.config/nvim/.vim_runtime/vimrcs/vimplugrc.vim
+' > ~/.config/nvim/init.vim
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
